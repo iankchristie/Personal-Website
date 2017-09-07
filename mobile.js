458221983bb12b616af8822f26b788a8dd90bbd8
iankchristie <iankchristie@gmail.com>
2017-09-07 08:35:15 -0700

    Mobile edits

--- mobile.js
+++ mobile.js
@@ -10,5 +10,22 @@ $(function() {
 
   if(isMobile) {
     console.log("Doing Mobile work");
+    $("#Tooltip").remove();
+    $("#InfoPane").addClass("mobile");
+    $("#Name").hide();
+    $("#Info").addClass("mobile");
+    $("#Description").addClass("mobile");
+    $("#Graph").addClass("mobile");
+    $("#Contact").addClass("mobile");
+    $("#Square").addClass("mobile");
+    $(".circle-title").addClass("mobile");
+
+    $("#Professional").removeClass("deg0");
+    $("#Professional").addClass("mobile");
+    $("#Professional").addClass("deg90");
+
+    $("#Personal").removeClass("deg180");
+    $("#Personal").addClass("deg270");
+    $("#Personal").addClass("mobile");
   }
 });
