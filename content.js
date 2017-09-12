5db02c4ca42b4a5d48dbbd4ded3ec723f5c5f044
iankchristie <iankchristie@gmail.com>
2017-09-12 12:27:05 -0700

    Grammar edits

--- content.js
+++ content.js
@@ -40,13 +40,13 @@ tabs["Google"] = {
 tabs["Chrome"] = {
     "heading": "Chrome",
     "image": "Images/Chrome.png",
-    "description": ["The Chrome browser was amazing to work on! It was my first experience working on a <i>civilization sized</i> product and the engineering on the team is phenomenal. I worked on an experimental project called <b>The Physical Web</b>, which was a way to introduce the <b>Internet Of Things</b> into Chrome. Specifically, I worked on the Chrome for Android."]
+    "description": ["The Chrome browser was amazing to work on! It was my first experience working on a <i>civilization sized</i> product and the engineering on the team is phenomenal. I worked on an experimental project called <b>The Physical Web</b>, which was a way to introduce the <b>Internet Of Things</b> into Chrome. Specifically, I worked on Chrome for Android."]
 };
 // FatBeacon
 tabs["FatBeacon"] = {
     "heading": "FatBeacon",
     "image": "Images/RaspberryPi3.jpg",
-    "description": ["Prototyped and optimized peer-to-peer webpage transfer over BLE from Raspberry Pi 3 to Android. Achieved a transfer rate of 32.8 KB/s (7.5x faster than previous attempts)."]
+    "description": ["I prototyped and optimized peer-to-peer webpage transfer over BLE from Raspberry Pi 3 to Android. I achieved a transfer rate of 32.8 KB/s (7.5x faster than previous attempts)."]
 };
 // Sharing
 tabs["Sharing"] = {
@@ -64,7 +64,7 @@ tabs["ShareSheet"] = {
 tabs["PrototypeApp"] = {
     "heading": "Physical Web Prototype App",
     "image": "Images/Prototype.png",
-    "description": ["I enhanced UX of Physical Web prototype app by moving work off of main thread to reduce dropped frames.<br/>",
+    "description": ["I enhanced UX of the Physical Web prototype app by moving work off of the main thread to reduce dropped frames.<br/>",
         "Also I designed and implemented a new settings page for the app that was more user friendly and intuitive.",
         "The source code is available on my <a target=\"blank\" href=\"https://github.com/iankchristie/physical-web\">Github</a>."
     ]
@@ -167,7 +167,7 @@ tabs["Research"] = {
     "description": ["During sophomore year I started my own computational neuroscience research project funded by an NIH grant. The goal of the project was to understand how a type of visual selectivity, called Direction Selectivity, developed. And to determine whether that development could take place entirely within the cortex, or if it had to rely on plasticity upstream in the brain.<br/>",
         "I created a generalizable model of a neuronal circuit that would take realistic input and determine an output as a firing rate. The firing rates of the system could be categorized. As a result each model could be thought of as an n-dimensional point in space, where each dimension was a parameter to the model and that point could be classified.",
         "So we scanned and classified the parameter space using Brandeis' Computational Cluster. Then we determined plasticity rules that would allow the model to go from one point in space to another simulating the training experience.<br/>",
-        "I found two models that the fit biological data and made measurable predictions. Wet lab epxeriments are currently verifying those models currently.<br/>",
+        "I found two models that fit the biological data and made measurable predictions. Wet lab experiments are currently verifying those models.<br/>",
         "The research became my senior honors thesis and was awared <i>Highest Honors</i> and <i>The Division of Science Award for Outstanding Scientific Research</i>. The research was published by the Journal of Neuroscience <a target=\"blank\" href=\"https://www.ncbi.nlm.nih.gov/pubmed/28515285\">here</a>. It received recognition from <a target=\"blank\" href=\"http://blogs.brandeis.edu/science/2017/06/05/learning-to-see/\">Brandeis</a> " +
          "and the <a target=\"blank\" href=\"https://www.facebook.com/JournalofNeurophysiology/photos/a.312797488894767.1073741828.290827221091794/830765037098007/?type=3\">Journal</a>. The source code can be found at my <a target=\"blank\" href=\"https://github.com/iankchristie/Neuroscience\">Github</a>."
     ]
@@ -224,7 +224,7 @@ tabs["iGel"] = {
         "The idea was to reduce the cost and increase the availability of an instrument to document the results of an important scientific procedure called Gel Electrophoresis. The procedure is complicated but the instruments needed aren't.",
         "<ul><li>Gel</li><li>DNA</li><li>Batteries and wire</li><li>A box with a light</li><li>Camera</li></ul>",
         "At the time a few companies sold the UV light box and camera and packed them with unnecessary features to inflate the cost to about $5,000. Because of the cost most departments only have one documentation set up, which creates a bottle neck to productivity. Most experiments only requires an image of the gel to validate the procedure and the extra features are only necessary for paper images.<br/>",
-        "To reduce the cost and increase productivity, our solution was to design a smaller UV box for the lab bench, so that an iphone could be placed on it to take the picture. Next I used iOS Core Image libraries to process the image. If a paper image of the gel was needed then the more advanced hardware could be used.<br/>",
+        "To reduce the cost and increase productivity, our solution was to design a smaller UV box for the lab bench, so that an iPhone could be placed on it to take the picture. Next I used iOS Core Image libraries to process the image. If a paper image of the gel was needed then the more advanced hardware could be used.<br/>",
         "The code can be found at my <a target=\"blank\" href=\"https://github.com/iankchristie/iGel\">Github</a>."
     ]
 };
@@ -281,7 +281,7 @@ tabs["BikeAndBuild"] = {
     "image": "Images/BnB.jpg",
     "description": ["Through service-oriented cross-country cycling trips, Bike & Build benefits affordable housing and empowers young adults to seek a lifetime of service and civic engagement.<br/>",
         "I participated as a rider biking from Providence, RI to Seattle, WA in the summer of 2013. It was an amazing summer and I made great friends doing it. The following semester I became the Habitat For Humanity chapter coordinator at my university.<br/>",
-        "I then lead a trip from Acadia, ME to New York City, NY in 2016. The position taught me a lot about leadership. I've raised $7,000 for affordable housing."
+        "I then led a trip from Acadia, ME to New York City, NY in 2016. The position taught me a lot about leadership. I've raised $7,000 for affordable housing."
     ]
 };
 // RAMROD
@@ -329,7 +329,7 @@ tabs["BoyScouts"] = {
     "heading": "Boy Scouts",
     "image": "Images/Scouts.jpg",
     "description": ["I'm an Eagle Scout and proud. Scouts taught me to be trustworthy, loyal, helpful, friendly, courteous, kind, obedient, cheerful, thrifty, brave, clean, and reverent.<br/>",
-        "In addition, scouts gave me some of the best friends and experiences I could have asked for.<br/>",
+        "In addition, Scouts gave me some of the best friends and experiences I could have asked for.<br/>",
         "* Picture above contains the whole crew except for David. He must been somewhere downstream."
     ]
 };
